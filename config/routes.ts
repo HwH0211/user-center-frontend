6f2536d45859d86c08cbeb25392f9f46243a2fa6
HwH0211 <1605779817@qq.com>
2023-10-05 15:58:27 +0800

    完成了登录、注册功能的前端页面，初步的用户管理页面

--- config/routes.ts
+++ config/routes.ts
@@ -2,19 +2,29 @@ export default [
   {
     path: '/user',
     layout: false,
-    routes: [{ path: '/user/login', component: './user/Login' }, { component: './404' }],
-  },
-  { path: '/welcome', icon: 'smile', component: './Welcome' },
-  {
-    path: '/admin',
-    icon: 'crown',
-    access: 'canAdmin',
     routes: [
-      { path: '/admin/sub-page', icon: 'smile', component: './Welcome' },
-      { component: './404' },
+      {
+        path: '/user', routes: [
+          {name: '登录', path: '/user/login', component: './user/Login'},
+          {name: '注册', path: '/user/register', component: './user/Register'}
+        ]
+      },
+      {component: './404'},
     ],
   },
-  { icon: 'table', path: '/list', component: './TableList' },
-  { path: '/', redirect: '/welcome' },
-  { component: './404' },
+  {path: '/welcome', name: '欢迎', icon: 'smile', component: './Welcome'},
+  {
+    path: '/admin',
+    name: '管理页',
+    icon: 'crown',
+    access: 'canAdmin',
+    component: './Admin',
+    routes: [
+      {path: '/admin/user-manage', name: '用户管理', icon: 'smile', component: './Admin/UserManage'},
+      {component: './404'},
+    ],
+  },
+  {name:'查询表格',icon: 'table', path: '/list', component: './TableList'},
+  {path: '/', redirect: '/welcome'},
+  {component: './404'},
 ];
